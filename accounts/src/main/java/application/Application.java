423b56e1c5b738985757185c599e5aae3b275cc0
root <root@wordpress.cs.rutgers.edu>
2017-12-04 13:49:24 -0500

    changes for Spring 2 version 2

--- accounts/src/main/java/application/Application.java
+++ accounts/src/main/java/application/Application.java
@@ -21,17 +21,10 @@ package application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 
 
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application.class);
-    }
+public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
